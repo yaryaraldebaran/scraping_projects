fd9ca6c64f8b02a8383b03f993e24141ee03048e
aldbrnstdy <aldebaranstudy97@gmail.com>
2023-12-11 05:54:01 +0700

    comm

--- scraping_sport_station/scraping_sport_station.py
+++ scraping_sport_station/scraping_sport_station.py
@@ -16,7 +16,7 @@ driver = webdriver.Chrome(service=service)
 sportst_utils = SportStatUtils(driver=driver)
 wait = WebDriverWait(driver, 10)
 driver.set_window_size(1920,1080)
-#url
+#urlk
 url = "https://www.sportsstation.id/men-footwear.html"
 
 driver.get(url)
